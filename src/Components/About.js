23868323bcebb7c9f8e8fd5c3191489fab033d6d
zahraginns@gmail.com <zahraginns@gmail.com>
2021-09-25 16:33:09 -0500

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,8 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
+    const bio2 = this.props.data.bio2;
+    const goal = this.props.data.goal;
     const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
@@ -24,13 +26,16 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="Zahra Bodaghi-Ginns Profile Pic"
               />
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
 
-              <p>{bio}</p>
+              {bio}<br/>
+              {bio2} <br/>
+              <div>{goal}</div>
+
               <div className="row">
                 <div className="columns contact-details">
                   <h2>Contact Details</h2>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -118,29 +118,22 @@ class Contact extends Component {
               </div>
 
               <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets</h4>
+                <h4 className="widget-title">Useful Websites</h4>
                 <ul id="twitter">
                   <li>
-                    <span>
-                      This is Photoshop's version of Lorem Ipsum. Proin gravida
-                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                      quis bibendum auctor, nisi elit consequat ipsum
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
+                    <span>    
+                      <a href="https://www.google.com">Google</a>
                     </span>
                     <b>
-                      <a href="./">2 Days Ago</a>
+                     
                     </b>
                   </li>
                   <li>
                     <span>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
+                      <a href="http://Yahoo.com">Yahoo</a>
                     </span>
                     <b>
-                      <a href="./">3 Days Ago</a>
+                      
                     </b>
                   </li>
                 </ul>
